8d19bffada4f20054ceaa3b29c735dc2ee3ca2a0
Shubhendu Shekhar <9253059+shekhar-shubhendu@users.noreply.github.com>
2020-11-09 10:11:48 +0100

    Update to delphi protocol
    #1

--- mini_sandbox_archive.sh
+++ mini_sandbox_archive.sh
@@ -25,11 +25,11 @@ start () {
              --time-between-blocks 0 \
              --timestamp-delay 0 \
              --no-baking \
-             --tezos-baker tezos-baker-006-PsCARTHA \
-             --tezos-endorser tezos-endorser-006-PsCARTHA \
-             --tezos-accuser tezos-accuser-006-PsCARTHA \
-             --protocol-hash PsCARTHAGazKbHtnKfLzQg3kms52kSRpgnDY982a9oYsSXRLQEb \
-             --protocol-kind Carthage \
+             --tezos-baker tezos-baker-007-PsDELPH1 \
+             --tezos-endorser tezos-endorser-007-PsDELPH1 \
+             --tezos-accuser tezos-accuser-007-PsDELPH1 \
+             --protocol-hash PsDELPH1Kxsxt8f9eWbxQeRxkjfbxoqM52jvs5Y5fBxWWh4ifpo \
+             --protocol-kind Delphi \
              --set-history-mode N000:archive
 }
 
